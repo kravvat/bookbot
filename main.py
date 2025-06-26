ea1e9a77927b6cf2fa320351b98099fa749e3905
kravvat <kacperstec3d@gmail.com>
2025-06-26 13:04:51 +0200

    Add feature for printing a full report

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-from stats import get_num_words, get_chars_dict
+from stats import get_num_words, get_chars_dict, get_chars_list
 
 
-def main():
+def main(): 
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
+    chars_list = get_chars_list(chars_dict)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} totals words")
     print("--------- Character Count -------")
-    # sorted list of dictionaries here
+    print_report(chars_list)
     print("============= END ===============")
 
 
 def get_book_text(path):
     with open(path) as file:
         return file.read()
+    
 
-# def sort_on()
+def sort_on(unsorted_list):
+    return unsorted_list["num"]
+
+
+def print_report(chars_list):
+    for i in range(0, len(chars_list)):
+        char_key = chars_list[i]['char']
+        if char_key.isalpha() == True:
+            print(f"{char_key}: {chars_list[i]['num']}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,16 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def get_chars_list(chars_dict):
+    chars_list = []
+    for char in chars_dict:
+        char_count = chars_dict[char] 
+        chars_list.append({'char': char, 'num': char_count})
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list

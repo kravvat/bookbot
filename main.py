44fe720c53315acbefc0320134cdc32217a2d672
kravvat <kacperstec3d@gmail.com>
2025-06-26 13:22:21 +0200

    Add comments to reinforce knowledge

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-from stats import get_num_words, get_chars_dict, get_chars_list
+from stats import get_num_words, get_chars_dict, get_chars_list # import functions defined in other file
 
 
-def main(): 
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_list = get_chars_list(chars_dict)
+def main(): # this will be executed on 'python3 main.py'
+    book_path = "books/frankenstein.txt" # path to file that we want to analyze
+    text = get_book_text(book_path) # file as a string
+    num_words = get_num_words(text) # word count
+    chars_dict = get_chars_dict(text) # dictionary with chars as keys and their count as values
+    chars_list = get_chars_list(chars_dict) # list that separates each char/count pair into individual dictionary
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
+    print(f"Analyzing book found at {book_path}...") # show which file is being analyzed
     print("----------- Word Count ----------")
-    print(f"Found {num_words} totals words")
+    print(f"Found {num_words} totals words") # print total number of words
     print("--------- Character Count -------")
-    print_report(chars_list)
+    print_report(chars_list) # print main report
     print("============= END ===============")
 
 
-def get_book_text(path):
+def get_book_text(path): # converts our file to string
     with open(path) as file:
         return file.read()
     
 
-def sort_on(unsorted_list):
+def sort_on(unsorted_list): # tells .sort() method that it should use value of a "num" key as a key
     return unsorted_list["num"]
 
 
-def print_report(chars_list):
+def print_report(chars_list): # shows main report while ignoring non-alphabetical chars
     for i in range(0, len(chars_list)):
         char_key = chars_list[i]['char']
         if char_key.isalpha() == True:
--- stats.py
+++ stats.py
@@ -1,9 +1,9 @@
-def get_num_words(text):
+def get_num_words(text): # calculates the word count
     words = text.split()
     return len(words)
 
 
-def get_chars_dict(text):
+def get_chars_dict(text): # dictionary with chars as keys and their count as values
     chars = {}
     for char in text:
         lowered = char.lower()
@@ -14,11 +14,11 @@ def get_chars_dict(text):
     return chars
 
 
-def sort_on(items):
+def sort_on(items): # this tells .sort() that it should use values from "num" key as a reference
     return items["num"]
 
 
-def get_chars_list(chars_dict):
+def get_chars_list(chars_dict): # packs char/count pairs into separate dictionaries and puts these dictionaries into a sorted list
     chars_list = []
     for char in chars_dict:
         char_count = chars_dict[char] 
